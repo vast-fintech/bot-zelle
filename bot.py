2836c75362222457340752a569f329ced23ae74e
vast-fintech <99835392+vast-fintech@users.noreply.github.com>
2022-04-20 17:18:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,7 +170,7 @@ def operate():
             print('Item encontrado')
             a=1
 
-        time.sleep(1)
+        time.sleep(5)
         cancelar = driver.find_element_by_xpath('//*[@id="root"]/div/main/div[1]/div[2]/div/div[3]/table/tbody/tr['+str(last+1)+']/td/div/div/div/div/div[2]/div[2]/div/button')
         cancelar.click()
         print('Operación CANCELADA con éxito')
